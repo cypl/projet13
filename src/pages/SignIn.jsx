5881ea31df68f6f829d194b553bade12980f7203
cypl <cyrille.plenchette@me.com>
2023-05-18 16:41:14 +0200

    add SignIn page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,2 +1,35 @@
-function SignIn(){}
+import { NavLink } from "react-router-dom"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+
+function SignIn(){
+    return(        
+        <div className="root-wrapper">
+            <Header />
+            <main className="main bg-dark">
+                <section className="sign-in-content">
+                    <i className="fa fa-user-circle sign-in-icon"></i>
+                    <h1>Sign In</h1>
+                    <form>
+                    <div className="input-wrapper">
+                        <label htmlFor="username">Username</label><input type="text" id="username" />
+                    </div>
+                    <div className="input-wrapper">
+                        <label htmlFor="password">Password</label><input type="password" id="password" />
+                    </div>
+                    <div className="input-remember">
+                        <input type="checkbox" id="remember-me" /><label htmlFor="remember-me">Remember me</label>
+                    </div>
+                    {/* PLACEHOLDER DUE TO STATIC SITE */}
+                    <NavLink to="/user" className="sign-in-button">Sign In</NavLink>
+                    {/* SHOULD BE THE BUTTON BELOW
+                    <!-- <button className="sign-in-button">Sign In</button> -->
+                     */}
+                    </form>
+                </section>
+                </main>
+            <Footer />
+        </div>
+        )
+}
 export default SignIn
