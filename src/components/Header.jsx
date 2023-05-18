4c47bb79cedb0c7bfd60e6f08a8c352d4410eeee
cypl <cyrille.plenchette@me.com>
2023-05-18 16:00:15 +0200

    fix Header navigation links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
+import { NavLink } from "react-router-dom"
+
 function Header(){
     return (
         <nav className="main-nav">
-            <a className="main-nav-logo" href="./index.html">
-            <img className="main-nav-logo-image" src="/assets/argentBankLogo.png" alt="Argent Bank Logo"/>
-            <h1 className="sr-only">Argent Bank</h1>
-            </a>
+            <NavLink to="/" className="main-nav-logo">
+                <img className="main-nav-logo-image" src="/assets/argentBankLogo.png" alt="Argent Bank Logo"/>
+                <h1 className="sr-only">Argent Bank</h1>
+            </NavLink>
             <div>
-            <a className="main-nav-item" href="./sign-in.html">
-                <i className="fa fa-user-circle"></i> Sign In
-            </a>
+                <NavLink to="/signin" className="main-nav-item">
+                    <i className="fa fa-user-circle"></i> Sign In
+                </ NavLink>
             </div>
         </nav>
     )
