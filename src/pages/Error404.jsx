cdcf20b2334d9f2154c728f3a5e260815f61e85e
cypl <cyrille.plenchette@me.com>
2023-05-18 18:05:43 +0200

    add Error404 page

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,2 +1,23 @@
-function Error404(){}
+import PropTypes from 'prop-types'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+function Error404({isLogged}){
+    return(
+        <div className="root-wrapper">
+            <Header isLogged={isLogged}/>
+            <main className="main bg-dark">
+                <p>Erreur 404</p>
+                {/* <p>Désolé, il n'y a rien sur cette page.</p>
+                <p>Retour à la page d'accueil</p> */}
+            </main>
+            <Footer/>
+        </div>
+    )
+
+}
 export default Error404
+
+Error404.propTypes = {
+    isLogged: PropTypes.bool,
+  }
