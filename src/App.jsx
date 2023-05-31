3c5f33a835110ce5e654634f0622c7457b30548c
cypl <cyrille.plenchette@me.com>
2023-05-31 10:11:40 +0200

    file structure changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider, UserProvider } from './utils/Context'
 import SiteTitle from './components/SiteTitle'
-import Router from './components/Router'
+import Router from './router/Router'
 
 function App() {
   return (
