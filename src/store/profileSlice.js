864e931e0dc44a6f5b8d89ddf40a9444212f3fe9
cypl <cyrille.plenchette@me.com>
2023-06-06 10:27:10 +0200

    add profileSlice to store

---
+++ src/store/profileSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    firstName: "",
+    lastName: "",
+};
+
+export const profileSlice = createSlice({
+  name: 'profile',
+  initialState,
+  reducers: {
+    setFirstName: (state, text) => {
+      state.firstName = text;
+    },
+    setLastName: (state, text) => {
+      state.lastName = text;
+    },
+  },
+});
+
+export const { setFirstName, setLastName } = profileSlice.actions;
+
+export default profileSlice.reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
 import loggerSlice from "./loggerSlice"
+import profileSlice from "./profileSlice"
 
 export const store = configureStore({
   reducer: {
-    logger: loggerSlice
+    logger: loggerSlice,
+    profile: profileSlice,
   },
 })
